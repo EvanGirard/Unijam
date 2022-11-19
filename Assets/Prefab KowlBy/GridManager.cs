dce6ac9fd90e84db7445e884de47b3ae0d4cc56d
KowlBy <piercurapro@hotmail.com>
2022-11-19 16:34:22 +0100

    Pour le build

---
+++ Assets/Prefab KowlBy/GridManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int length=10;
+    public int heigth=10;
+    public int bottom=-5;
+    public int left=-5;
+    [SerializeField] GameObject floor;
+    [SerializeField] GameObject mur;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<length; i++){
+            for(int j=0; j<heigth; j++){
+                Instantiate(floor, new Vector3(0.5f+left+i,0.5f+bottom+j,0), new Quaternion());
+            }
+        }
+        for(int i=0; i<length; i++){
+            Quaternion orient=new Quaternion();
+            Instantiate(mur, new Vector3(left-0.5f,i+0.5f+bottom,0),orient);
+            orient.eulerAngles = new Vector3(0,0,-90);
+            Instantiate(mur, new Vector3(left+0.5f+i,0.5f+bottom+heigth,0),orient);
+            orient.eulerAngles = new Vector3(0,0,180);
+            Instantiate(mur, new Vector3(left+length+0.5f,i+0.5f+bottom,0),orient);
+            orient.eulerAngles = new Vector3(0,0,90);
+            Instantiate(mur, new Vector3(left+0.5f+i,-0.5f+bottom,0),orient);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Prefab KowlBy/Monstre.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monstre : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void takedamage(int x){
+        Debug.Log(x);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
